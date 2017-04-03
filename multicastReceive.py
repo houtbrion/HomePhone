7aec3a7e273b3cedd404284de30e150faeba2ac2
houtbrion <houtbrion@gmail.com>
2017-04-03 21:54:20 +0900

    ネットワークのテストコードを追加した．

--- multicastReceive.py
+++ multicastReceive.py
@@ -3,6 +3,36 @@
 from __future__ import print_function
 import socket
 from contextlib import closing
+import netifaces
+
+# IPアドレスのリストの取得
+# 出力例 : [{'addr': '127.0.0.1', 'netmask': '255.0.0.0', 'peer': '127.0.0.1'}]
+def getIpAddress():
+    ipList=[]
+    for iface_name in netifaces.interfaces():
+        iface_data = netifaces.ifaddresses(iface_name).get(netifaces.AF_INET))
+        iface_data['dev'] = iface_name
+        print(iface_data)
+        ipList.extend(iface_data)
+    return ipList
+
+# IPアドレスのリストをフィルタして，特定のアドレスを取り出す
+# 出力例 : '127.0.0.1'
+def addressFilter(ipList,filterString):
+    for addressInfo in ipList:
+        addr=addressInfo['addr']
+        if filterString in addr:
+            return addr
+    return ""
+
+# IPアドレスのリストをフィルタして，特定のデバイスのアドレスを取り出す
+# 出力例 : '127.0.0.1'
+def deviceFilter(ipList,filterString):
+    for addressInfo in ipList:
+        dev=addressInfo['dev']
+        if filterString in addr:
+            return addressInfo['addr']
+    return ""
 
 def main():
     #local_address   = '192.168.1.8' # 受信側のPCのIPアドレス
@@ -20,8 +50,16 @@ def main():
                              + socket.inet_aton(local_address))
 
         while True:
-            print(sock.recv(bufsize))
+            #print(sock.recv(bufsize))
+            data, addr = sock.recv(bufsize)
+            print " data = " , data
+            print " addr = " , addr
     return
 
 if __name__ == '__main__':
+    ip=getIpAddress()
+    local_ip=addressFilter(ip,'192.168')
+    print local_ip
+    local_ip=deviceFilter(ip,'eth')
+    print local_ip
     main()
--- networkTest.py
+++ networkTest.py
@@ -9,9 +9,10 @@ import netifaces
 def getIpAddress():
     ipList=[]
     for iface_name in netifaces.interfaces():
-        iface_data = netifaces.ifaddresses(iface_name)
-        print(iface_data.get(netifaces.AF_INET))
-        ipList.extend(iface_data.get(netifaces.AF_INET))
+        iface_data = netifaces.ifaddresses(iface_name).get(netifaces.AF_INET))
+        iface_data['dev'] = iface_name
+        print(iface_data)
+        ipList.extend(iface_data)
     return ipList
 
 # IPアドレスのリストをフィルタして，特定のアドレスを取り出す
@@ -23,9 +24,20 @@ def addressFilter(ipList,filterString):
             return addr
     return ""
 
+# IPアドレスのリストをフィルタして，特定のデバイスのアドレスを取り出す
+# 出力例 : '127.0.0.1'
+def deviceFilter(ipList,filterString):
+    for addressInfo in ipList:
+        dev=addressInfo['dev']
+        if filterString in addr:
+            return addressInfo['addr']
+    return ""
+
 
 if __name__ == '__main__':
     ip=getIpAddress()
     local_ip=addressFilter(ip,'192.168')
+    print local_ip
+    local_ip=deviceFilter(ip,'eth')
     print local_ip
     val = pyping.ping('192.168.1.1')
